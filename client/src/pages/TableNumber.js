999dd3248a607aa73e50e5b83f44e81ec2274644
wjhunter5315 <wjhunter5315@gmail.com>
2020-01-16 14:37:44 -0800

    tablefrom put on tablenumber page

--- client/src/pages/TableNumber.js
+++ client/src/pages/TableNumber.js
@@ -1,10 +1,20 @@
 import React from "react";
+import TableNumberForm from "../components/TableNumberForm.js";
 
 function TableNumber() {
     return(
         <div>
-            THIS IS THE TABLE NUMBER PAGE.
+            <div>
+                ENTER THE 4 - DIGIT CODE
+            </div>
+            <div>
+                (Found below the QR Code on the Table)
+            </div>
+            <div>
+                <TableNumberForm />
+            </div>
         </div>
+        
     )
 }
 
