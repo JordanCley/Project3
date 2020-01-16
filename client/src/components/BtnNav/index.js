08fdbef0d8ad8141801fa637efabbc0ad2201086
Robert Parsons <rmpcoding@gmail.com>
2020-01-16 14:46:32 -0800

    Working on BtnNav

---
+++ client/src/components/BtnNav/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function BtnNav() {
+  return (
+    <Button href="#">Link</Button> //need to add routes to this
+  )
+}
+
+export default BtnNav
+
+// This button nav is unnecessary 
