53c0b6e2239af14880cb5c87826b6fbb5d263a4b
Robert Parsons <rmpcoding@gmail.com>
2020-01-17 12:06:28 -0800

    adds grid and layout to MyOrders.js page

--- client/src/pages/MyOrders.js
+++ client/src/pages/MyOrders.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 function MyOrders() {
-    return (
-        <div>
-            This is the View My Orders Page.
-        </div>
-    )
-};
+  return (
+    <Container fluid>
+      <Row>
+        <img
+          className="d-block w-100 mx-auto"
+          src="https://bbimenus.blob.core.windows.net/items/03501/ccapp-03501-2x.png"
+          alt="background image"
+          style={{opacity: 0.5}}
+        />
+      </Row>
+      <Row className="text-center mt-4 mb-4">
+        <Col lg={12}>
+          <p>This is the View My Orders Page.</p>
+          <Button variant="outline-danger mr-2" size="lg" href="/menu">
+            Back To Menu
+          </Button>
+          <Button variant="outline-success ml-2" size="lg" href="/view-check">
+            Place Order
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default MyOrders;
