394ad6203f7b8bbdea85004fcabc82c360991e90
wjhunter5315 <wjhunter5315@gmail.com>
2020-01-23 10:16:27 -0800

    confirm card component

--- client/src/pages/ConfirmPay.js
+++ client/src/pages/ConfirmPay.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import '../index.css';
+import ConfirmPaymentCard from "../components/ConfirmPaymentCard";
 
 function ConfirmPay() {
   return (
     <div>
       This page is the confirm checkout page
+      <ConfirmPaymentCard />
       <Link to="/card-info">
         <Button>Cancel</Button>
       </Link>
