c004f44cb248c7a2b4feb5f5de108ed454782dcb
Glenny <55863951+anthonychry@users.noreply.github.com>
2020-01-22 21:53:16 -0800

    added components to 'ViewCheck.js'

--- client/src/pages/ViewCheck.js
+++ client/src/pages/ViewCheck.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { OrderContext } from "../utils/context/OrderContext.js"
 import { Link } from "react-router-dom";
 import OrderField from "../components/OrderField/OrderField";
-import Button, {
-  GoToCardInfoBtn,
-  GoToMyOrdersBtn
-} from "../components/Buttons/Buttons";
+import { GoToCardInfoBtn, GoToMyOrdersBtn } from "../components/Buttons/Buttons";
+import { Container, Row, Col } from "react-bootstrap";
 import '../index.css';
 
 function ViewCheck() {
+  const { orderState } = useContext(OrderContext);
+
+
   return (
     <div>
       <OrderField />
+        <Col lg={4}>
+            <span>Tax: ${orderState.total * (orderState.tax / 100)}</span>
+        </Col>
+        <Col lg={4}>
+            <span>Grand Total: ${orderState.grandTotal}</span>
+        </Col>
+
       <GoToCardInfoBtn />
       <GoToMyOrdersBtn />
     </div>
