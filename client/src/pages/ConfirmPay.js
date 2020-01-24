1208415387a0cffc4280f38de334cb949be71760
Glenny <55863951+anthonychry@users.noreply.github.com>
2020-01-23 20:45:06 -0800

    added last minute touches to CSS

--- client/src/pages/ConfirmPay.js
+++ client/src/pages/ConfirmPay.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import OrderField from "../components/OrderField/OrderField";
 import "../index.css";
@@ -20,28 +20,36 @@ function ConfirmPay() {
     return <div>Loading...</div>
   }
   return (
-    <div className="confirm-pay-page bg-table-in-vintage-restaurant">
+    <div className="confirm-pay-page bg-table-in-vintage-restaurant confirm-pay">
       {openCheckState.items.length ? (
         <div>
           {openCheckState.items.map(product => (
-            <div key={product._id}>
+            <Row key={product._id} className="receipt-items">
               <p>{product.productName}</p>
               <p>{product.quantity}</p>
               <p>{product.price}</p>
-            </div>
+            </Row>
           ))}
         </div>
       ) : (
         <p>No items in cart</p>
       )}
-
-      <p>Tax: {openCheckState.tax}%</p>
-      <p>Sub Total: ${openCheckState.total}</p>
-      <p>Tip: {openCheckState.gratuity}%</p>
-      <p>Grand Total: ${openCheckState.grandTotal.toFixed(2)}</p>
+      <Col lg={4}>
+        <span>Tax: {openCheckState.tax}%</span>
+      </Col>
+      <Col lg={4}>
+        <span>Sub Total: ${openCheckState.total}</span>
+      </Col>
+      <Col lg={4}>
+        <span>Tip: {openCheckState.gratuity}%</span>
+      </Col>
+      <Col lg={4}>
+        <span>Grand Total: ${openCheckState.grandTotal.toFixed(2)}</span>
+      </Col>
       {/*<ConfirmPaymentCard /> */}
+      <div className="confirm-pay-buttons"></div>
       <Link to="/card-info">
-        <Button>Cancel</Button>
+        <Button className="left-button">Cancel</Button>
       </Link>
       <Link >
         <Button onClick={handlePayClick}>
